875c2040a16c44ec6d02b1e4d76c90f4b455c59c
GuruMahan <iosdeve2022@gmail.com>
2026-01-15 22:24:40 +0530

    Initial Commit

---
+++ Bike_Purchasing/Bike_PurchasingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bike_PurchasingApp.swift
+//  Bike_Purchasing
+//
+//  Created by Guru  Mahan on 15/01/26.
+//
+
+import SwiftUI
+
+@main
+struct Bike_PurchasingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
